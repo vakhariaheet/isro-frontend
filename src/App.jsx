1327abcf93e630246c836788c7fa47e038eba833
Heet Vakharia <heetkv@gmail.com>
2024-07-18 23:37:45 +0530

    Added Docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import io from 'socket.io-client'
 function App() {
-  const [count, setCount] = useState(0)
+  const [ isRecording, setIsRecording ] = useState(false)
+  const [ transcription, setTranscription ] = useState('')
+  const socketRef = useRef()
+  const mediaRecorderRef = useRef()
+
+  useEffect(() => {
+    socketRef.current = io('http://localhost:3004');
+
+    socketRef.current.on('transcription', (data) => {
+      setTranscription(prev => prev + ' ' + data)
+    });
+
+    return () => {
+      socketRef.current.disconnect()
+    }
+
+  }, [])
+
+  const startRecording = async () => {
+    console.log('start recording')
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorderRef.current = new MediaRecorder(stream);
+    mediaRecorderRef.current.ondataavailable = event => {
+      const reader = new FileReader();
+      reader.readAsDataURL(event.data);
+      reader.onloadend = () => {
+        const base64data = reader.result.split(',')[ 1 ];
+        console.log(base64data)
+        socketRef.current.emit('audio_chunk', { chunk: base64data });
+      };
+    };
+    mediaRecorderRef.current.start(250); // Adjust the interval as needed
+    setIsRecording(true);
+  };
+
+  const stopRecording = () => {
+    mediaRecorderRef.current.stop();
+    setIsRecording(false);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={isRecording ? stopRecording : startRecording}>
+          {isRecording ? 'Stop Recording' : 'Start Recording'}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Transcription: {transcription}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
